889d73e4b5ec45be3a99eeb8c63a4492c48af55e
Valera-Neg <nehovora.val@gmail.com>
2022-11-10 18:36:02 -0500

    first commit

---
+++ better.js
@@ -0,0 +1,27 @@
+function greet(lang) {
+  return langs[lang]||langs['english'];
+}
+
+var langs = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+};
+
+console.log(greet('english')) //> 'Welcome'
+console.log(greet('dutch')) //> 'Welkom'
+console.log(greet('IP_ADDRESS_INVALID')) //> 'Welcome'
